dc97faa0e94c2eaafe34895ec4683fff8433d325
Christopher Wilson <ckwilson4@gmail.com>
2024-07-26 17:07:38 -0500

    Update cosmos_test

--- src/cosmos_test/Program.cs
+++ src/cosmos_test/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 internal class Program
 {
     private static string _cosmosConnectionString;
+    private static string _databaseName;
+    private static string _containerName;
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Add Configuration");
@@ -14,7 +17,9 @@ internal class Program
 
         try
         {
-            Task.Run(async () => await CreateItem()).Wait();
+            for (int i = 0; i < 10; i++)
+                Task.Run(async () => await CreateItem()).Wait();
+
         }
         catch (AggregateException ex)
         {
@@ -29,15 +34,24 @@ internal class Program
                .AddEnvironmentVariables()
                .Build();
 
+        // get connection string
         _cosmosConnectionString = config.GetConnectionString("CosmosDB") ??
-                throw new NullReferenceException("CosmosDB Connection String is null");
+                throw new NullReferenceException("CosmosDB Connection String is not set in appSettings");
+
+        // get database name
+        _databaseName = config["CosmosDatabaseName"] ??
+                        throw new NullReferenceException("CosmosDatabaseName is not set in appSettings");
+
+        // get container name
+        _containerName = config["CosmosContainerName"] ??
+                throw new NullReferenceException("CosmosContainerName is not set in appSettings");
     }
 
     static async Task CreateItem()
     {
         //variables
-        var databaseName = "Context";
-        var containerName = "ContextDiagram";
+        //var databaseName = "Context";
+        //var containerName = "ContextDiagram";
         var insertId = Guid.NewGuid().ToString();
 
         ContextDiagram item = new()
@@ -52,10 +66,10 @@ internal class Program
             connectionString: _cosmosConnectionString);
 
         // Create a new database
-        Database database = client.GetDatabase(databaseName);
+        Database database = client.GetDatabase(_databaseName);
 
         // Create a new container
-        Container container = database.GetContainer(containerName);
+        Container container = database.GetContainer(_containerName);
 
         // Create a new item
         ContextDiagram upsertedItem = await container.UpsertItemAsync<ContextDiagram>(
